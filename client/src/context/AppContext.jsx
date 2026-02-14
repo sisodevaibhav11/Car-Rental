046d497465bc23b810d9534cb730c154d2e7ecf4
vaibhav <sisodevaibhav11@gmail.com>
2026-02-14 17:29:59 +0530

    renamed file

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+
+export const AppContext=createContext();
+
+export const AppProvider=({children})=>{
+
+    const value={
+
+    }
+    return 
+    (
+        <AppContext.Provider value={value}>
+           {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext=()=>{
+    return useContext(AppContext)
+}
